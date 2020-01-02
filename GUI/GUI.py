91b2ba2eb962574cdf17f50cfe0f23a067c38bff
lvdongxu <lvdongxu@sjtu.edu.cn>
2020-01-02 23:58:54 +0800

    单行GUI更改尝试

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -13,7 +13,7 @@
 import tkinter as tk
 
 class Person:
-    def __init__(self, root):
+    def __init__(self, root, name_of_guy, row_of_guy):
         '''
         @description: 
         @param : 
@@ -23,9 +23,10 @@ class Person:
         root.geometry('600x400')
         self.Time = tk.IntVar() 
         self.Time.set(0)
-        tk.Label(root, textvariable=self.Time).pack()
-        tk.Button(root, text='+1', command=self.plus).pack()
-        tk.Button(root, text='-1', command=self.minus).pack()
+        tk.Label(root, text=name_of_guy).grid(row=row_of_guy, column=2, rowspan=3, columnspan=4, padx=10)
+        tk.Label(root, textvariable=self.Time).grid(row=row_of_guy, rowspan=3, column=7, padx=40)
+        tk.Button(root, text='+1', command=self.plus).grid(row=row_of_guy, rowspan=3, column=8)
+        tk.Button(root, text='-1', command=self.minus).grid(row=row_of_guy, rowspan=3, column=9)
 
 
     def plus(self):
@@ -49,7 +50,7 @@ class Person:
         self.Time.set(cur_time)
 
 root = tk.Tk()
-test_tk = Person(root)
+test_tk = Person(root, '吕东旭', 1)
 root.mainloop()
 
 
