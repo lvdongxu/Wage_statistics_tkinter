b83f433592e068cc325cf263fb764b2f65be488d
lvdongxu <lvdongxu@sjtu.edu.com>
2020-01-04 21:14:07 +0800

    基本计数、工资计算和GUI界面搭建工作已经基本完成

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -19,15 +19,36 @@ class Person:
         @param : 
         @return: 
         '''
-        # root.title('文科处助管工资统计')
-        # root.geometry('600x400')
+        # 这里是上午班，下午班和加班次数的整数变量定义
         self.morning_time = tk.IntVar() 
         self.morning_time.set(0)
-        # tk.Label(_frame, text='shit').pack(side=tk.LEFT)
-        tk.Label(_frame, textvariable=self.morning_time, height=2, width=10).pack(side=tk.LEFT)
-        tk.Button(_frame, text='+1', command=self.morning_plus, height=2, width=4).pack(side=tk.LEFT)
-        tk.Button(_frame, text='-1', command=self.morning_minus, height=2, width=4).pack(side=tk.LEFT)
-
+        self.afternoon_time = tk.IntVar();
+        self.afternoon_time.set(0)
+        self.extra_time = tk.IntVar();
+        self.extra_time.set(0)
+        self.wage = tk.IntVar();
+        self.wage.set(0)
+        # 一些基本参数类似于列扩展，x方向的pad
+        set_columnspan = 8
+        set_padx = 4
+        # 上午班次数统计和UI组件定义
+        tk.Label(_frame, text="上午值班次数", height=2, width=10, bg='red', fg='white').grid(row=0, column=0, columnspan=2*set_columnspan, padx=set_padx)
+        tk.Label(_frame, textvariable=self.morning_time, height=2, width=10).grid(row=1, column=0, columnspan=2*set_columnspan, padx=set_padx)
+        tk.Button(_frame, text='+1', command=self.morning_plus, height=2, width=4).grid(row=2, column=0, columnspan=set_columnspan)
+        tk.Button(_frame, text='-1', command=self.morning_minus, height=2, width=4).grid(row=2, column=set_columnspan, columnspan=set_columnspan)
+        # 下午班次数统计和UI组件定义
+        tk.Label(_frame, text="下午值班次数", height=2, width=10, bg='blue', fg='white').grid(row=0, column=2*set_columnspan, columnspan=2*set_columnspan, padx=set_padx)
+        tk.Label(_frame, textvariable=self.afternoon_time, height=2, width=10).grid(row=1, column=2*set_columnspan, columnspan=2*set_columnspan, padx=set_padx)
+        tk.Button(_frame, text='+1', command=self.afternoon_plus, height=2, width=4).grid(row=2, column=2*set_columnspan, columnspan=set_columnspan)
+        tk.Button(_frame, text='-1', command=self.afternoon_minus, height=2, width=4).grid(row=2, column=3*set_columnspan, columnspan=set_columnspan)
+        # 加班次数统计和UI组件定义
+        tk.Label(_frame, text="加班次数", height=2, width=10, bg='green', fg='white').grid(row=0, column=4*set_columnspan, columnspan=2*set_columnspan, padx=set_padx)
+        tk.Label(_frame, textvariable=self.extra_time, height=2, width=10).grid(row=1, column=4*set_columnspan, columnspan=2*set_columnspan, padx=set_padx)
+        tk.Button(_frame, text='+1', command=self.extra_plus, height=2, width=4).grid(row=2, column=4*set_columnspan, columnspan=set_columnspan)
+        tk.Button(_frame, text='-1', command=self.extra_minus, height=2, width=4).grid(row=2, column=5*set_columnspan, columnspan=set_columnspan)
+        # 总的工资
+        tk.Label(_frame, text='总的工资', height=2, width=10, bg='black', fg='white').grid(row=0, column=6*set_columnspan, columnspan=4*set_columnspan, padx=set_padx)
+        tk.Label(_frame, textvariable=self.wage , height=2, width=10, bg='black', fg='white').grid(row=2, column=6*set_columnspan, columnspan=4*set_columnspan, padx=set_padx)
 
     def morning_plus(self):
         '''
@@ -38,6 +59,7 @@ class Person:
         cur_morning_time = self.morning_time.get()
         cur_morning_time += 1
         self.morning_time.set(cur_morning_time)
+        self.wage_cal()
 
     def morning_minus(self):
         '''
@@ -48,15 +70,73 @@ class Person:
         cur_morning_time = self.morning_time.get()
         cur_morning_time -= 1
         self.morning_time.set(cur_morning_time)
+        self.wage_cal()
+
+    def afternoon_plus(self):
+        '''
+        @description: 
+        @param : 
+        @return: 
+        '''
+        cur_afternoon_time = self.afternoon_time.get()
+        cur_afternoon_time += 1
+        self.afternoon_time.set(cur_afternoon_time)
+        self.wage_cal()
+
+    def afternoon_minus(self):
+        '''
+        @description: 
+        @param : 
+        @return: 
+        '''
+        cur_afternoon_time = self.afternoon_time.get()
+        cur_afternoon_time -= 1
+        self.afternoon_time.set(cur_afternoon_time)
+        self.wage_cal()
+
+    def extra_plus(self):
+        '''
+        @description: 
+        @param : 
+        @return: 
+        '''
+        cur_extra_time = self.extra_time.get()
+        cur_extra_time += 1
+        self.extra_time.set(cur_extra_time)
+        self.wage_cal()
+
+    def extra_minus(self):
+        '''
+        @description: 
+        @param : 
+        @return: 
+        '''
+        cur_extra_time = self.extra_time.get()
+        cur_extra_time -= 1
+        self.extra_time.set(cur_extra_time)
+        self.wage_cal()
+
+    def wage_cal(self):
+        '''
+        @description: 
+        @param : 
+        @return: 
+        '''
+        cur_morning_time = self.morning_time.get()
+        cur_afternoon_time = self.afternoon_time.get()
+        cur_extra_time = self.extra_time.get()
+        cur_wage = cur_morning_time * 75 + cur_afternoon_time * 100 + cur_extra_time * 50
+        self.wage.set(cur_wage)
+
 
 root = tk.Tk()
 root.title('文科处助管工资统计')
-root.geometry('600x400')
+# root.geometry('400x500')
 
 name = ['吕东旭','王晓情','李启开']
 frame_list = []
 for name_i in name:
-    _frame = tk.LabelFrame(height=1000, width=6000, text=name_i)
+    _frame = tk.LabelFrame(text=name_i)
     Person(_frame)
     _frame.pack(side=tk.TOP)
 
