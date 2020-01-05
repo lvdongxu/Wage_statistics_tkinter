1206b742f52f90bccc27741bd220d599b5e9f195
lvdongxu <lvdongxu@sjtu.edu.com>
2020-01-05 22:00:08 +0800

    pandas 1月5日晚在实验室的尝试，似乎有苗头了

--- GUI/pd_excel.py
+++ GUI/pd_excel.py
@@ -10,20 +10,23 @@
 '''
 
 # here put the import lib
+import numpy as np
 import pandas as pd
 import sys
 import io
 sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')#改变默认输出的标准编码
 
 
-df = pd.read_excel('D:\\onedrive\\文科处助管\\工资统计\\wage_11.16_12.15.xls')
+# df = pd.read_excel('D:\\onedrive\\文科处助管\\工资统计\\wage_11.16_12.15.xls') # 笔记本工资表路径
+df = pd.read_excel('F:/PythonProject/Wage_statistics_tkinter/GUI/wage_11.16_12.15.xls') # 实验室主机工资表路径
 height, width = df.shape
-print(df.iloc[3,1])
+# print(df.iloc[3,1])
+print(df.loc[2])
 
-dict_key = ['姓名', '上午值班次数', '下午值班次数', '加班小时数', '总酬金']
-dict_value = 
-# data_list = []
-# for _h in range(0,height):
-#     _data_row = []
-#     for _w in range(1, width+1):
-#         _data_row.append()
+# dict_key = ['姓名', '上午值班次数', '下午值班次数', '加班小时数', '总酬金']
+# dict_value = ['王晓情', 2, 3, 4, 5]
+# p = dict(zip(dict_key, dict_value))
+# print(p)
+# 
+# df['加班小时数'][df['姓名'] == '王晓情'] = p['加班小时数']
+# df.to_excel('F:/PythonProject/Wage_statistics_tkinter/GUI/wage_11.16_12.15.xls', sheet_name='Sheet1', index=True, header=True)
